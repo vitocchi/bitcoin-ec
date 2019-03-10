3dfe65779d2f9fa12e6a476851a3a76b18f55223
Kosuke Ito <ito-kosuke0502@g.ecc.u-tokyo.ac.jp>
2019-03-10 13:12:11 +0900

    rename client to public

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('client'))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('index.html'))
 
